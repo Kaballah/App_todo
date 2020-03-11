dc698d2f5a3ced1787ef61f930138fb351f9adbc
Minace McKaballah <minace@localhost.localdomain>
2020-03-11 10:07:42 +0300

    Creating other functional component related to React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useReducer } from 'react';
+import AddNoteForm from './components/AddNoteForm'
 import './App.css';
 
-function App() {
+const noteReducer = (state, action) => {
+  switch (action.type) {
+    case 'POPULATE_NOTES':
+      return action.notes;;
+      case 'ADD_NOTE':
+        return [
+          ...state,
+          { title: action.title, body: action.body}
+        ]
+        case 'REMOVE_NOTE':
+          return state.filter((note) => note.title !== action.title)
+      default:
+        return state
+  }
+}
+
+const App = () => {
+  const [notes, dispatch] = useReducer(noteReducer, []); // dispatch affects the reducer
+  
+  const removeNotes = (title) => {
+    dispatch({
+      type: 'REMOVE_NOTE',
+      title
+    })
+  }
+
+  useEffect(() => {  // useEffects picks things from the localstorage
+    const notes = JSON.parse(localStorage.getItem('notes'))
+    if (notes) {
+      dispatch({ type: 'POPULATE_NOTES', notes })
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+        {notes.map((note) => {
+          return (
+            <div>
+              <h3>{note.title}</h3>
+              <p>{note.body}</p>
+              <button onClick={() => removeNotes(note.title)}>Delete</button>
+            </div>
+          )
+        })}
+        <AddNoteForm dispatch={dispatch}/>
     </div>
-  );
+  )
 }
 
 export default App;
